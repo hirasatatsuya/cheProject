f3e9c6a3e49ad5b366bb2055a1fac7d8a182236b
ngnlesn33 <son.nl215131@gmail.com>
2023-08-26 18:42:54 +0700

    Detail page #1

--- che_project/tailwind.config.js
+++ che_project/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./che_project_app/templates/**/*.html"],
+  content: [
+    "./che_project_app/templates/**/*.html",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
