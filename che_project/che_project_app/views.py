01c71f8db139fd01f72e01a2ce92527f07d21acf
hirasatatsuya <omametan1229@gmail.com>
2023-08-28 14:53:59 +0700

    fix フォルダ構成

--- che_project/che_project_app/views.py
+++ che_project/che_project_app/views.py
@@ -5,29 +5,25 @@ from django.shortcuts import render
 # Please add below.
 def car_list(request):
     """The page for car list"""
-    return render(request, "che_project_app/car_list.html")
-
+    return render(request, "car_list.html")
 
 def chat(request, id):
     """The page for car list"""
     context = {
         "id": id,
     }
-    return render(request, "che_project_app/chat.html", context)
-
+    return render(request, "chat.html", context)
 
 def detail_view(request, id):
     # Retrieve data based on the id parameter and pass it to the template
     context = {
         "id": id,
     }
-    return render(request, "che_project_app/detail.html", context)
-
+    return render(request, "detail.html", context)
 
 def info_update(request):
     """The page for editing user intent"""
-    return render(request, "che_project_app/info_update.html")
-
+    return render(request, "info_update.html")
 
 def index(request):
     return HttpResponse("Hello World")
