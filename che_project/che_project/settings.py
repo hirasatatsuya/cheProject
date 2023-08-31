f05cf32410dbe62393ff8a4c1737ad82f8be69ea
ngnlesn33 <son.nl215131@gmail.com>
2023-08-31 15:29:09 +0700

    Fix info update page @ngnlesn33

--- che_project/che_project/settings.py
+++ che_project/che_project/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "scar_db",
         "USER": "postgres",
-        "PASSWORD": "12345",
+        "PASSWORD": "Son11223344",
         "HOST": "localhost",
         "PORT": "5432",
     }
